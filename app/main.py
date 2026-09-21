df2a28c860256b39dc2793828541aed605374f05
Sighmantaneous <150013312+Sighmantaneous@users.noreply.github.com>
2026-09-21 15:54:49 +0000

    Added Delete function and endpoint

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,  HTTPException, status 
+from fastapi import FastAPI,  HTTPException, status, Response
 
 from app.schemas import UserCreate 
  
@@ -22,6 +22,19 @@ def add_user(new_user: UserCreate):
                 status_code=status.HTTP_409_CONFLICT, 
                 detail="A user with this user_id already exists", 
             ) 
-
     users.append(new_user)
     return new_user
+
+@app.delete("/api/users/{user_id}", status_code=status.HTTP_204_NO_CONTENT) 
+def delete_user(user_id: int): 
+
+    for index, existing_user in enumerate(users): 
+        if existing_user.user_id == user_id: 
+            users.pop(index) 
+            return Response(status_code=status.HTTP_204_NO_CONTENT) 
+
+
+    raise HTTPException( 
+        status_code=status.HTTP_404_NOT_FOUND, 
+        detail="User not found", 
+    ) 
